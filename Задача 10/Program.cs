898547aa2c741aac81b9e0a75fdc1833f41e1b7e
vladimir <tarskij-vladimir@yandex.ru>
2023-09-25 08:11:07 +0300

    added  Task10

---
+++ Задача 10/Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+Console.WriteLine("Введите трехзначное число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+ int a1 = a % 100;
+ int a2 = a1 / 10;
+ 
+
+Console.WriteLine($"Трехзначное число {a} ,без первой цифры и третьей будет {a2}");
