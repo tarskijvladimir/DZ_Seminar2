f0528724beac2b11c0eb40d8e70b733af62e52ef
vladimir <tarskij-vladimir@yandex.ru>
2023-09-25 09:11:10 +0300

    added changes in 15

--- Задача 15/Program.cs
+++ Задача 15/Program.cs
@@ -5,31 +5,31 @@ Console.Write("Введите цифру:   ");
 int number = Convert.ToInt32(Console.ReadLine());
 if(number == 1) 
 {
-    Console.WriteLine("Сегодня понедельник, этот день не выходной");
+    Console.WriteLine("нет");
 }
-else if(number == 2)  //доп условие, чтоб при нахождении числа – программа завершилась. При обычном if программа идет дальше – иногда это надо, чтоб вывести несколько чисел
+else if(number == 2)  
 {
-    Console.WriteLine("Сегодня вторник, этот день не выходной");
+    Console.WriteLine("нет");
 }
 else if(number == 3)
 {
-    Console.WriteLine("Сегодня среда, этот день не выходной");
+    Console.WriteLine("нет");
 }
 else if(number == 4)
 {
-    Console.WriteLine("Сегодня четверг, этот день не выходной");
+    Console.WriteLine("нет");
 }
 else if(number == 5)
 {
-    Console.WriteLine("Сегодня пятница, этот день не выходной");
+    Console.WriteLine("нет");
 }
 else if(number == 6)
 {
-    Console.WriteLine("Сегодня суббота, выходной");
+    Console.WriteLine("Да");
 }
 else if(number == 7)
 {
-    Console.WriteLine("Сегодня воскресенье, выходной");
+    Console.WriteLine("Да");
 }
 else
 {
